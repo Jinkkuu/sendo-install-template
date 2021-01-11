c47368a5eaafe2aa7d5dbf060b3b6872039ca153
Mojeg <49614252+mojeg@users.noreply.github.com>
2021-01-11 01:31:08 -0500

    Made a new feature to name your files

--- install.py
+++ install.py
@@ -1,10 +1,12 @@
 import os
+v1='cat files/package/* >'
+v1+=open('filename.lvd','r').read()
 print('DO NOT USE THIS FOR VIDEO TRANSFERS OR PHOTO TRANSFERS, IT WILL CORRUPT IT!\n\nDo you want to install it?')
 q1=input('[y/n]:')
 if q1 == 'y':
   os.system('unzip package.lvd')
   os.system('rm package.lvd')
-  os.system('cat files/package/* >program.py')
+  os.system(v1)
   os.system('rm files/package/ -r')
   print('Finished!')
 else:
