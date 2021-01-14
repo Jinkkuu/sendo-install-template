2ca7f7445e3fb55b21c9dac8cbc4adee36c5f0f6
Mojeg <49614252+mojeg@users.noreply.github.com>
2021-01-14 18:29:46 -0500

    Create installbeta.py

---
+++ installbeta.py
@@ -0,0 +1,15 @@
+import os
+print('Checking OS (To make it compatable)')
+os.system('clear')
+v1='cat files/package/* >'
+v1+=open('filename.lvd','r').read()
+print('Sendo Package Manager 1.1\n\nDo you want to install it?')
+q1=input('[y/n]:')
+if q1 == 'y':
+  os.system('unzip package.lvd')
+  os.system('rm package.lvd')
+  os.system(v1)
+  os.system('rm files/package/ -r')
+  print('Finished!')
+else:
+  print('Goodbye!')
